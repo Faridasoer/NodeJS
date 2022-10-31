4e7e62d9f2e9ca0e98193d97fabd1eb27fe1d104
niorism <117099069+niorism@users.noreply.github.com>
2022-10-31 23:25:26 +0700

    Update niorism.js

--- niorism.js
+++ niorism.js
@@ -1,137 +1,39 @@
-var choices = ["telunjuk", "kelingking", "jempol"]; 
- var i = Math.floor(Math.random() * 3); 
- var NPC = choices[i]; 
- var Point player = 0; 
- var Point NPC = 0; 
- function score(){ 
-     var score_div = document.getElementById("score").innerHTML = Point player + " - " + Point NPC; 
- } 
- setInterval(score, 50); 
- function convert(word){ 
-     if(word === "telunjuk") return '<i class="far fa-hand-telunjuk"></i>'; 
-     if(word === "kelingking") return '<i class="far fa-hand-kelingking"></i>'; 
-     return '<i class="far fa-hand-jempol"></i>' 
- } 
- function game(UserChoice){ 
-     var box = document.getElementById("challenge"); 
-     box.style.display = "inline-flex"; 
-     var userDiv = document.getElementById("YourObject"); 
-     userDiv.innerHTML = convert(UserChoice); 
-     var comDiv = document.getElementById("ComObject"); 
-     comDiv.innerHTML = convert(NPC); 
-     if(UserChoice === "telunjuk" && NPC === "kelingking" || UserChoice === "kelingking" && NPC === "jempol" || UserChoice === "jempol" && NPC === "telunjuk"){ 
-         win(UserChoice); 
-     } 
-     else if(UserChoice === NPC){ 
-         draw(UserChoice); 
-     } 
-     else{ 
-         lose(UserChoice); 
-     } 
-     function continuGame(){ 
-         i = Math.floor(Math.random() * 3); 
-         NPC = choices[i]; 
-         box.style.display = "none"; 
-     } 
-     setTimeout(continuGame, 1200); 
- } 
- function win(bn){ 
-     Point player++; 
-     document.getElementById("who").innerHTML = "kamu menang!"; 
-     var bn = document.getElementById(bn); 
-     bn.classList.remove("bn"); 
-     bn.classList.add("green"); 
-     setTimeout(() => { 
-         bn.classList.add("bn"); 
-         bn.classList.remove("green"); 
-     }, 1200); 
- } 
- function draw(bn){ 
-     document.getElementById("who").innerHTML = "Seimbang"; 
-     var bn = document.getElementById(bn); 
-     bn.classList.remove("bn"); 
-     bn.classList.add("gray"); 
-     setTimeout(() => { 
-         bn.classList.add("bn"); 
-         bn.classList.remove("gray"); 
-     }, 1200); 
- } 
- function lose(bn){ 
-     Point NPC++; 
-     document.getElementByIdvar choices = ["telunjuk", "kelingking", "jempol"]; 
- var i = Math.floor(Math.random() * 3); 
- var NPC = choices[i]; 
- var Point player = 0; 
- var Point NPC = 0; 
- function score(){ 
-     var score_div = document.getElementById("score").innerHTML = Point player + " - " + Point NPC; 
- } 
- setInterval(score, 50); 
- function convert(word){ 
-     if(word === "telunjuk") return '<i class="far fa-hand-telunjuk"></i>'; 
-     if(word === "kelingking") return '<i class="far fa-hand-kelingking"></i>'; 
-     return '<i class="far fa-hand-jempol"></i>' 
- } 
- function game(UserChoice){ 
-     var box = document.getElementById("challenge"); 
-     box.style.display = "inline-flex"; 
-     var userDiv = document.getElementById("YourObject"); 
-     userDiv.innerHTML = convert(UserChoice); 
-     var comDiv = document.getElementById("ComObject"); 
-     comDiv.innerHTML = convert(NPC); 
-     if(UserChoice === "telunjuk" && NPC === "kelingking" || UserChoice === "kelingking" && NPC === "jempol" || UserChoice === "jempol" && NPC === "telunjuk"){ 
-         win(UserChoice); 
-     } 
-     else if(UserChoice === NPC){ 
-         draw(UserChoice); 
-     } 
-     else{ 
-         lose(UserChoice); 
-     } 
-     function continuGame(){ 
-         i = Math.floor(Math.random() * 3); 
-         NPC = choices[i]; 
-         box.style.display = "none"; 
-     } 
-     setTimeout(continuGame, 1200); 
- } 
- function win(bn){ 
-     Point player++; 
-     document.getElementById("who").innerHTML = "kamu menang!"; 
-     var bn = document.getElementById(bn); 
-     bn.classList.remove("bn"); 
-     bn.classList.add("green"); 
-     setTimeout(() => { 
-         bn.classList.add("bn"); 
-         bn.classList.remove("green"); 
-     }, 1200); 
- } 
- function draw(bn){ 
-     document.getElementById("who").innerHTML = "Seimbang"; 
-     var bn = document.getElementById(bn); 
-     bn.classList.remove("bn"); 
-     bn.classList.add("gray"); 
-     setTimeout(() => { 
-         bn.classList.add("bn"); 
-         bn.classList.remove("gray"); 
-     }, 1200); 
- } 
- function lose(bn){ 
-     Point NPC++; 
-     document.getElementById("who").innerHTML = "Kamu Kalah...."; 
-     var bn = document.getElementById(bn); 
-     bn.classList.remove("bn"); 
-     bn.classList.add("red"); 
-     setTimeout(() => { 
-         bn.classList.add("bn"); 
-         bn.classList.remove("red"); 
-     }, 1200); 
- }who").innerHTML = "Kamu Kalah...."; 
-     var bn = document.getElementById(bn); 
-     bn.classList.remove("bn"); 
-     bn.classList.add("red"); 
-     setTimeout(() => { 
-         bn.classList.add("bn"); 
-         bn.classList.remove("red"); 
-     }, 1200); 
- }
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
+const randomUseragent = require('random-useragent');
+
+
+var uname = readlineSync.question('[+] Masukan Username :');
+var pw = readlineSync.question('[+] Masukan Password :');
+var mail = readlineSync.question('[+] Masukan Email : ')
+const $options = {
+    waitUntil: 'networkidle2'
+	};
+
+
+(async () => {
+    var usgent = randomUseragent.getRandom(function (ua) {
+        return ua.osName === 'Windows';
+	});
+
+  var browser = await puppeteer.launch ({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto('https://github.com/join', $options);
+
+  const username = await page.type('#user_login',uname);
+  const email  = await page.type('#user_email',mail);
+  const password = await page.type('#user_password',pw);
+  await page.select('select#all_emails');
+  await page.select('#signup_button');
+
+  try {
+    await page.waitForNavigation($options);
+    console.log('Berhasil Membuat Akun');
+    await browser.close();
+  } catch (error) {
+    console.log('Gagal Membuat Akun');
+    await browser.close();
+  }
+})();
